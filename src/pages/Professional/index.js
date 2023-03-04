e34b8138041a5d2f8335e31af8a0924d01649674
gabriel <dgabridb3@gmail.com>
2023-03-04 17:19:21 -0300

    página profissional pronta

--- src/pages/Professional/index.js
+++ src/pages/Professional/index.js
@@ -1,10 +1,24 @@
-
+import {Header} from "../../components/Header"
+import {Footer} from "../../components/Footer"
+import Img from "../../image/profissional1.svg"
+import { Container,Content,ContainerImg} from "./styled"
 
 export function Professional(){
 
     return(
         <>
-        Professional
+        <Header/>
+        <Container>
+                 <Content>
+                    <h1>Profissional</h1>
+                    <h2>Buscamos recrutar pessoas profissionais da saúde que entendam as necessidades e se comprometam com o bem-estar da comunidade LGBTQIAPN+</h2>
+                 </Content>
+            <ContainerImg>
+                    <img src={Img} alt="grupo de médicos se olhando"/>
+            </ContainerImg>
+        
+         </Container>
+        <Footer/>
         </>
     )
 }
